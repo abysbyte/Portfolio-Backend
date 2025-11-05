47073dadf8055f3d7673d439919243ad6a1bb14c
'Vishal <156448228+evyvishal@users.noreply.github.com>
2025-11-05 18:20:40 +0530

    adding a SecurityConfig file

---
+++ Portfoliobackdend/src/main/java/com/portfolio/bknd/configuration/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.portfolio.bknd.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/api/resume/download").permitAll()
+					.anyRequest().permitAll()
+					)
+			.httpBasic(httpBasic -> httpBasic.disable());
+		return http.build();
+	}
+}
